4ae5c01058720da644a3971595535e11faf691c6
Jenny Kim <kimj1905@gmail.com>
2021-09-14 14:13:41 -0400

    commit tree f3d86ee4a63f8ca600858d7ddc573eaa56899b4a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your functions here
+function map(arr, func) {
+  const newArr = [];
+  for (const num of arr) {
+    newArr.push(func(num));
+  }
+  return newArr;
+}
+
+function reduce(arr, func, x) {
+  let aggregate;
+  if (x) {
+    aggregate = x;
+    for (const num of arr) {
+      aggregate = func(num, aggregate);
+    }
+    // return aggregate;
+  } else {
+    aggregate = arr[0];
+    const newArr = arr.slice(1, arr.length);
+    for (const num of newArr) {
+      aggregate = func(num, aggregate);
+    }
+    // return aggregate;
+  }
+  return aggregate;
+}
